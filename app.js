311f38453913f273d0dcfe14784980ca00f2ac65
Ajoy-paul11 <ajoypaul8875@gmail.com>
2024-02-05 00:04:21 +0530

    Formatting the code

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ let winningPattern = [[0, 1, 2], [0, 3, 6], [0, 4, 8], [1, 4, 7], [2, 5, 8], [2,
 
 boxes.forEach((box) => {
     box.addEventListener('click', () => {
-        console.log("button is clicked");
+
         if (playerX) {
             box.innerText = "X";
             playerX = false;
@@ -27,10 +27,12 @@ const resetGame = () => {
     enabledboxes()
 
 }
+
 const anotherGame = () => {
     enabledboxes()
     hide.style.display = 'none'
 }
+
 const enabledboxes = () => {
     for (let box of boxes) {
         box.disabled = false;
@@ -41,6 +43,7 @@ const enabledboxes = () => {
     hide.classList.remove('m-top')
 
 }
+
 const disabledboxes = () => {
     for (let box of boxes) {
         box.disabled = true;
@@ -54,17 +57,16 @@ const callWinner = (winner) => {
     msg.innerText = `Winner is ${winner}`
     hide.style.display = 'block';
 }
+
 const checkWinner = () => {
     for (let pattern of winningPattern) {
-        // console.log(pattern[0], pattern[1], pattern[2]);
-        // console.log(boxes[pattern[0]], boxes[pattern[1]], boxes[pattern[2]]);
+
         let pos1val = boxes[pattern[0]].innerText;
         let pos2val = boxes[pattern[1]].innerText;
         let pos3val = boxes[pattern[2]].innerText;
 
         if (pos1val !== "" && pos2val !== "" && pos3val !== "") {
             if (pos1val === pos2val && pos2val === pos3val) {
-                console.log("Winner");
                 disabledboxes()
                 callWinner(pos1val)
                 reSet.classList.add('hide')
